25b8b0891d914121162487150b0769d287416126
chouti <chouti@gmail.com>
2010-11-22 14:54:28 +0800

    fix bugs with manganese.js

--- js/manganese.js
+++ js/manganese.js
@@ -1,4 +1,4 @@
-function make_base_auth(user,password){
+function make_base_auth(user,pass){
 	var tok = user + ':' + pass;
 	var hash = Base64.encode(tok);
 	return "Basic " + hash;
@@ -7,8 +7,11 @@ function make_base_auth(user,password){
 var auth = make_base_auth('manganese2010@qq.com','choutishiwo');
 var url = 'https://api.doit.im/v1/tasks';
 
-$.getJSON({url, 
-	beforeSend : function(req){
+$.ajax({
+	url: url,
+	method: 'GET',
+	beforeSend: function(req){
 		req.setRequestHeader('Authorization', auth);
-	}
+	},
+	dataType: "json"
 });

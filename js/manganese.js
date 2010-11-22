ea1f74d1e40521e222515543ac3407c7f9fbb7ce
chouti <chouti@gmail.com>
2010-11-22 14:40:20 +0800

    add basic http authorization call in Manganese.js

--- js/manganese.js
+++ js/manganese.js
@@ -1,3 +1,14 @@
-$.getJSON('https://api.doit.im/v1/tasks'
-	
-);
+function make_base_auth(user,password){
+	var tok = user + ':' + pass;
+	var hash = Base64.encode(tok);
+	return "Basic " + hash;
+}
+
+var auth = make_base_auth('manganese2010@qq.com','choutishiwo');
+var url = 'https://api.doit.im/v1/tasks';
+
+$.getJSON({url, 
+	beforeSend : function(req){
+		req.setRequestHeader('Authorization', auth);
+	}
+});

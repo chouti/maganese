c2b1019e0f6a24652939cfee106e18e6e4a43896
chouti <chouti@gmail.com>
2010-12-11 12:51:51 +0800

    add complete function

--- js/doit.js
+++ js/doit.js
@@ -5,6 +5,9 @@ function	get_user_info(){
 }
 
 function	get_user_tasks(){
+	$('#intime').empty();
+	$('#overdue').empty();
+	$('#next').empty();
 	$.getJSON(tasks, function(data){
 		$.each(data.entries, function(i,item){
 			var task_title=item.title;
@@ -15,14 +18,15 @@ function	get_user_tasks(){
 				var start_at=start_at_array[0];
 				var new_pair={task_id:task_title};
 				if(start_at==today){
-					$('#intime').append('<li><input type="image" id="complete_task" src="img/complete.png" value="'+(task_id)+'"><span id="origin_task">' + task_title + '</span></li>');}
+					$('#intime').append('<li><input type="image" name="complete_task" src="img/complete.png" value="'+(task_id)+'"><span id="origin_task">' + task_title + '</span></li>');}
 					else if(start_at<today){
-						$('#overdue').append('<li><input type="image" id="complete_task" src="img/complete.png" value="'+(task_id)+'"><span id="origin_task">' + task_title + '</span></li>');};
+						$('#overdue').append('<li><input type="image" name="complete_task" src="img/complete.png" value="'+(task_id)+'"><span id="origin_task">' + task_title + '</span></li>');};
 				if(start_at==tomorrow){
-					$('#next').append('<li><input type="image" id="complete_task" src="img/complete.png" value="'+(task_id)+'"><span id="origin_task">' + task_title + '</span></li>');}
+					$('#next').append('<li><input type="image" name="complete_task" src="img/complete.png" value="'+(task_id)+'"><span id="origin_task">' + task_title + '</span></li>');}
 			}
 		})
 	});
+	
 }
 
 function post_user_tasks(){
@@ -30,9 +34,7 @@ function post_user_tasks(){
 	var newtask_start_at_option=$("select[name='start_at']").val();
 	if(newtask_start_at_option=="today"){newtask_start_at=today}else{newtask_start_at=tomorrow};
 	newtask=JSON.stringify({'title':newtask_title,'start_at':newtask_start_at});
-	$.post(tasks, newtask, function(){
-		get_user_tasks();
-	});
+	$.post(tasks, newtask);
 }
 
 function show_add_task(){
@@ -50,19 +52,18 @@ function show_add_task(){
 	});
 }
 
-function complete_user_task(){
+function complete_user_task(origin_id){
 	$.getJSON(tasks, function(data){
-		var tasksobj={};
-		var entriesobj=jQuery.parseJSON(data.entries);
-		for(var i=entriesobj.length;i--;){
-			var entry=entriesobj[i];
-			items[entry.id]=entry;
-			$('#complete_task').click(function() {
-				var item=items[this.id];
-				alert(item.title);
-			});
+		for(var i=0; i<data.entries.length; i++){
+			if(data.entries[i].id==origin_id){
+				var entry=data.entries[i];
+				entry.completed=today;
+				var new_entry=JSON.stringify(entry);
+				console.log(new_entry);
+				$.post(tasks, new_entry);
+				return;
+			}
 		}
-		
 	});
 }
 
@@ -72,5 +73,12 @@ $(document).ready(function() {
 	$('#new_task').submit(function(){
 	post_user_tasks();
 	});
-	complete_user_task();
+	setTimeout(function(){
+		$('input[name="complete_task"]').click(function(){
+			var origin_id=$(this).attr('value');
+			console.log(origin_id);
+			complete_user_task(origin_id);
+			$(this).parent('li').slideUp();
+		});
+		},800);
 });
